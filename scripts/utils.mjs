abf4c5f4495e5a765dfc5b3994450355c9ffe169
jmalan210 <>
2026-01-26 22:22:20 -0700

    dynamic header and footer working

---
+++ scripts/utils.mjs
@@ -0,0 +1,27 @@
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.innerHTML = template;
+  if (callback) {
+    callback(data);
+  }
+}
+
+export async function loadTemplate(path) {
+  const response = await fetch(path);
+  return await response.text();
+}
+
+export async function loadHeaderFooter(){
+    const templateHeader = await loadTemplate("../partials/header.html");
+    const templateFooter = await loadTemplate("../partials/footer.html");
+
+    const header = document.querySelector("#dynamic-header");
+    const footer = document.querySelector("#dynamic-footer");
+    
+
+    renderWithTemplate(templateHeader, header, null);
+    renderWithTemplate(templateFooter, footer);
+        
+    const year = document.querySelector("#current-year");
+    year.textContent = new Date().getFullYear();
+
+}
