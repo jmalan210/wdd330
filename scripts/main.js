ea5aa962f48d740af8dba6c341d348c8710443f9
jmalan210 <>
2026-02-10 15:09:51 -0700

    logic added so bird of the day doesn't reload with the DOM

--- scripts/main.js
+++ scripts/main.js
@@ -9,11 +9,19 @@ document.addEventListener("DOMContentLoaded", async () => {
 
 async function loadBirdOfTheDay() {
     try {
-        const res = await fetch("https://server-4abf.onrender.com/api/us-notable-birds");
-        if (!res) throw new Error("Failed to fetch bird");
+        //checks cache to see if there is already a bird for today
+        const todayDate = new Date().toISOString().slice(0, 10);
+        const stored = JSON.parse(localStorage.getItem('birdOfTheDay') || '{}');
+        let bird;
+        if (stored.date === todayDate) {
+            bird = stored.bird;
+        } else {
+            const res = await fetch("https://server-4abf.onrender.com/api/us-notable-birds");
+            if (!res) throw new Error("Failed to fetch bird");
 
-        const bird = await res.json();
-        // console.log("Fetched bird:", bird);
+            bird = await res.json();
+            localStorage.setItem('birdOfTheDay', JSON.stringify({ date: todayDate, bird }));
+        }
         const birdDiv = document.getElementById("birdOfDay");
         const bod = new BirdOfDay(bird, birdDiv);
         await bod.init();
