0b1b7a8f66e6d4fd066b940d941c2aaef676ed5a
jmalan210 <>
2026-02-15 09:29:21 -0700

    css for bird lib cards

--- scripts/BirdLibrary.mjs
+++ scripts/BirdLibrary.mjs
@@ -6,6 +6,12 @@ export default class BirdLibrary {
         this.listElement = listElement;
         this.birds = JSON.parse(localStorage.getItem("seenBirds")) || [];
 
+        this.listElement.addEventListener("click", e => {
+            if (e.target.classList.contains("remove-bird")) {
+                this.removeBirdFromLibrary(e.target.dataset.code);
+            }
+        })
+
     }
     
     async render() {
@@ -29,15 +35,16 @@ export default class BirdLibrary {
                             <strong>${formatDate(new Date(note.date))}: </strong> ${note.text}: 
                             <button class="delete-note">X</button></li>`).join("") : "";
             
-             birdListItem.innerHTML = `
-                        <img src="${birdImg}" alt="${b.comName}" width="100">
+            birdListItem.innerHTML = `
+                        <button class="remove-bird" data-code=${b.speciesCode}>X</button>
+                        <img src="${birdImg}" alt="${b.comName}" width="150">
                         <p><strong>Name:</strong> ${b.comName}</p>
                        <p><strong>Scientific Name:</strong><em> ${b.sciName}</em></p>
                        <p><strong>Date Recorded:</strong> ${formatDate(b.dateRecorded)}</p>
                        <p><strong>Notes:</strong></p>
                        <ul id="notes-${b.speciesCode}">${existingNotes}</ul>
-                       <textarea id="note-${b.speciesCode}" placeholder="enter notes about this sighting"></textarea>
-                       <button id="save-${b.speciesCode}">Save Note</button>
+                       <textarea id="note-${b.speciesCode}" class="note-text-area" placeholder="enter notes about this sighting"></textarea>
+                       <button id="save-${b.speciesCode}" class="save-note">Save Note</button>
                        
                     `
                     this.listElement.appendChild(birdListItem);
@@ -46,9 +53,11 @@ export default class BirdLibrary {
                         this.saveNoteForBird(b.speciesCode);
                     });
             
+                   
+            
                 const notesList = birdListItem.querySelector(`#notes-${b.speciesCode}`);
-        notesList.addEventListener("click", e => {
-            if (e.target.classList.contains("delete-note")) {
+                notesList.addEventListener("click", e => {
+                if (e.target.classList.contains("delete-note")) {
                 const li = e.target.closest("li");
                 const noteIndex = li.dataset.noteIndex;
                 this.deleteNoteForBird(b.speciesCode, noteIndex)
@@ -56,7 +65,22 @@ export default class BirdLibrary {
         });
         }
         
-}
+    }
+    
+    removeBirdFromLibrary(speciesCode) {
+        const li = this.listElement.querySelector(`.remove-bird[data-code="${speciesCode}"]`).closest("li");
+        li.classList.add("removing");
+        setTimeout(() => {
+            this.birds = this.birds.filter(b => b.speciesCode !== speciesCode);
+            this.updateLocalStorage();
+            li.remove();
+
+        }, 500);
+       
+
+
+    }
+
     saveNoteForBird(speciesCode) {
         
         const bird = this.birds.find(b => b.speciesCode === speciesCode);
