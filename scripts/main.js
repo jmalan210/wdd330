1b316613f948471fcec34ff0e778dc6fe44ce65b
jmalan210 <>
2026-09-14 19:33:41 -0600

    disabled home page due to BoD function offline

--- scripts/main.js
+++ scripts/main.js
@@ -3,32 +3,32 @@ import { loadHeaderFooter, initHamburgerMenu } from "./utils.mjs";
 
 document.addEventListener("DOMContentLoaded", async () => {
     loadHeaderFooter();
-    await loadBirdOfTheDay();
+    // await loadBirdOfTheDay();
     initHamburgerMenu();
 })
 
 
-async function loadBirdOfTheDay() {
-    try {
-        //checks cache to see if there is already a bird for today
-        const todayDate = new Date().toISOString().slice(0, 10);
-        const stored = JSON.parse(localStorage.getItem('birdOfTheDay') || '{}');
-        let bird;
-        if (stored.date === todayDate) {
-            bird = stored.bird;
-        } else {
-            const res = await fetch("https://server-4abf.onrender.com/api/us-notable-birds");
-            if (!res) throw new Error("Failed to fetch bird");
+// async function loadBirdOfTheDay() {
+//     try {
+//         //checks cache to see if there is already a bird for today
+//         const todayDate = new Date().toISOString().slice(0, 10);
+//         const stored = JSON.parse(localStorage.getItem('birdOfTheDay') || '{}');
+//         let bird;
+//         if (stored.date === todayDate) {
+//             bird = stored.bird;
+//         } else {
+//             const res = await fetch("https://server-4abf.onrender.com/api/us-notable-birds");
+//             if (!res) throw new Error("Failed to fetch bird");
 
-            bird = await res.json();
-            localStorage.setItem('birdOfTheDay', JSON.stringify({ date: todayDate, bird }));
-        }
-        const birdDiv = document.getElementById("birdOfDay");
-        const bod = new BirdOfDay(bird, birdDiv);
-        await bod.init();
+//             bird = await res.json();
+//             localStorage.setItem('birdOfTheDay', JSON.stringify({ date: todayDate, bird }));
+//         }
+//         const birdDiv = document.getElementById("birdOfDay");
+//         const bod = new BirdOfDay(bird, birdDiv);
+//         await bod.init();
 
-    } catch (err) {
-        console.error(err);
-        document.getElementById("birdOfDay").textContent = "Error loading bird";
-    }
-} 
+//     } catch (err) {
+//         console.error(err);
+//         document.getElementById("birdOfDay").textContent = "Error loading bird";
+//     }
+// } 
